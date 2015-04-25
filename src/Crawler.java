16c118a5df2b6a80440468fae8b1db7c5d229ffa
Pritom Ahmed <pritom.11@gmail.com>
2015-04-25 14:21:33 -0700

    Stopped unused file creation in writeToFile function
    Now only creates a file when crc value is different. Also checks for
    unique crc value outside the writeToFile(..) function.

--- src/Crawler.java
+++ src/Crawler.java
@@ -28,7 +28,7 @@ public class Crawler extends Thread {
     private static Random random = new Random();
     private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) " +
             "Chrome/13.0.782.112 Safari/535.1";
-    private List<String> links = new LinkedList<String>();
+    List<String> links = new ArrayList<>();
     static Set<Long> crclib = new ConcurrentSkipListSet<>();
 
     public Crawler(String startUrl) {
@@ -50,8 +50,8 @@ public class Crawler extends Thread {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                if (pagesToBeVisited.size() < (MAX_PAGES_TO_CRAWL / 10000)) {
-                    addUrls(getLinks());
+                if (pagesToBeVisited.size() < 1000 || pagesToBeVisited.size() + links.size() < 100000) {
+                    addUrls(links);
                 }
             }
             System.out.println(String.format("**Done** Visited %s web page(s)", pagesVisited.size()));
@@ -73,23 +73,29 @@ public class Crawler extends Thread {
 
     CRC32 crc = new CRC32();
 
-    public List<String> getLinks() {
-        return links;
-    }
-
     public void crawl(String currentUrl) throws IOException {
         try {
             if (RobotExclusionUtil.robotsShouldFollow(currentUrl)) {
                 pagesVisited.add(currentUrl);
                 long startTime = System.currentTimeMillis();
 
-                Connection connection = Jsoup.connect(currentUrl).userAgent(USER_AGENT);
-                Document htmlDocument = connection.get();
+                Document htmlDocument = null;
+                try {
+                    htmlDocument = Jsoup.connect(currentUrl).userAgent(USER_AGENT).get();
+                } catch (OutOfMemoryError ignored) {
+                    System.out.println(currentUrl);
+                }
                 long endTime = System.currentTimeMillis();
                 long totalTime = endTime - startTime;
                 if (htmlDocument.text() != null) {
                     startTime = System.currentTimeMillis();
-                    writeToFile(htmlDocument);
+                    crc.update(htmlDocument.text().getBytes());
+                    long cval = crc.getValue();
+                    crc.reset();
+                    if (!crclib.contains(cval)) {
+                        crclib.add(cval);
+                        writeToFile(htmlDocument);
+                    }
                     endTime = System.currentTimeMillis();
 //                    System.out.println(currentUrl + " time elapsed in connection "
 //                            + totalTime + " time elapsed in writing " + (endTime - startTime)
@@ -111,28 +117,22 @@ public class Crawler extends Thread {
     private void writeToFile(Document htmlDocument) throws IOException {
         //String title = "".equals(htmlDocument.title().trim()) ? htmlDocument.head().text().replaceAll("\\W+", "")
         //        : htmlDocument.title().trim().replaceAll("\\W+", "");
-        File file = new File("output/" + random.nextLong() + ".txt");
 
+        File file = new File("output/" + random.nextLong() + ".txt");
         // if file doesnt exists, then create it
         if (!file.exists()) {
             file.createNewFile();
         } else {
             System.out.println("********************************* " + file.getName());
         }
-
-        crc.update(htmlDocument.text().getBytes());
-        long cval = crc.getValue();
-        crc.reset();
-        if (!crclib.contains(cval)) {
-            crclib.add(cval);
-            FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.append(htmlDocument.title());
-            bw.newLine();
-            bw.append(htmlDocument.text());
-            bw.newLine();
-            bw.close();
-        }
+        FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.append(htmlDocument.title());
+        bw.newLine();
+        bw.append(htmlDocument.text());
+        bw.newLine();
+        bw.close();
+        fw.close();
     }
 
     private String nextUrl() {
